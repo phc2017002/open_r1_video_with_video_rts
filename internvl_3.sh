738e6b5ad27ab890b0b32cd75f8c2a50fc353eb5
Aniket Pal <60142017+phc2017002@users.noreply.github.com>
2025-08-05 08:49:14 +0530

    Add files via upload

---
+++ internvl_3.sh
@@ -0,0 +1,35 @@
+# --- basic wandb tags ---------------------------------------------------------
+export WANDB_PROJECT=InternVL3-8B-Video-GRPO
+export WANDB_NAME=llava-video-4k-remove-formatreward-matchletterreward-f16
+
+# --- create output dir --------------------------------------------------------
+mkdir -p /data/wangxd/ckpt/$WANDB_PROJECT/$WANDB_NAME
+
+# --- launch -------------------------------  ------------------------------------
+CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node=4 \
+    --nnodes=1 \
+    --node_rank=0 \
+    --master_addr=127.0.0.1 \
+    --master_port=12352 \
+    src/open_r1_video/grpo.py \
+    --deepspeed scripts/zero3_offload.json \
+    --output_dir /data/wangxd/ckpt/$WANDB_PROJECT/$WANDB_NAME \
+    --model_name_or_path /data/wangxd/models/InternVL3-8B \
+    --dataset_name xxx \
+    --jsonl_path /home/user/wangxd/open-r1-multimodal/data/LLaVA-Video-large-swift-origin.jsonl \
+    --max_prompt_length 8192 \
+    --learning_rate 1e-6 \
+    --beta 0.1 \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 1 \
+    --logging_steps 1 \
+    --bf16 \
+    --torch_dtype bfloat16 \
+    --data_seed 42 \
+    --report_to wandb \
+    --gradient_checkpointing true \
+    --attn_implementation flash_attention_2 \
+    --num_train_epochs 1 \
+    --run_name $WANDB_NAME \
+    --save_steps 10 \
+    --save_only_model true
